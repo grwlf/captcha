e723477cc6df62561c1f61afd11ff1a353b86102
Sergey Mironov <grrwlf@gmail.com>
2015-06-06 07:57:56 +0000

    sanity checks during open/read

--- captcha.c
+++ captcha.c
@@ -1,12 +1,13 @@
 
 const int gifsize;
-void captcha(unsigned char im[70*200], unsigned char l[6]);
+int captcha(unsigned char im[70*200], unsigned char l[6]);
 void makegif(unsigned char im[70*200], unsigned char gif[gifsize]);
 
 #include <unistd.h>
 #include <stdint.h>
 #include <fcntl.h>
 #include <string.h>
+#include <errno.h>
 
 static int8_t *lt[];
 const int gifsize=17646;
@@ -158,18 +159,28 @@ static void filter(unsigned char im[70*200]) {
 
 static const char *letters="abcdafahijklmnopqrstuvwxyz";
 
-void captcha(unsigned char im[70*200], unsigned char l[6]) {
+int captcha(unsigned char im[70*200], unsigned char l[6]) {
 	unsigned char swr[200];
 	uint8_t s1,s2;
 
 	int f=open("/dev/urandom",O_RDONLY);
-	read(f,l,5); read(f,swr,200); read(f,dr,sizeof(dr)); read(f,&s1,1); read(f,&s2,1);
-	close(f);
-
-	memset(im,0xff,200*70); s1=s1&0x7f; s2=s2&0x3f; l[0]%=25; l[1]%=25; l[2]%=25; l[3]%=25; l[4]%=25; l[5]=0;
-	int p=30; p=letter(l[0],p,im,swr,s1,s2); p=letter(l[1],p,im,swr,s1,s2); p=letter(l[2],p,im,swr,s1,s2); p=letter(l[3],p,im,swr,s1,s2); letter(l[4],p,im,swr,s1,s2);
-	dots(im); blur(im); filter(im); line(im,swr,s1); 
-	l[0]=letters[l[0]]; l[1]=letters[l[1]]; l[2]=letters[l[2]]; l[3]=letters[l[3]]; l[4]=letters[l[4]];
+	if(f > 0) {
+		int ret = read(f,l,5)+read(f,swr,200)+read(f,dr,sizeof(dr))+read(f,&s1,1)+read(f,&s2,1);
+		if(ret == 5 + 200 + sizeof(dr) + 1 + 1) {
+			memset(im,0xff,200*70); s1=s1&0x7f; s2=s2&0x3f; l[0]%=25; l[1]%=25; l[2]%=25; l[3]%=25; l[4]%=25; l[5]=0;
+			int p=30; p=letter(l[0],p,im,swr,s1,s2); p=letter(l[1],p,im,swr,s1,s2); p=letter(l[2],p,im,swr,s1,s2); p=letter(l[3],p,im,swr,s1,s2); letter(l[4],p,im,swr,s1,s2);
+			dots(im); blur(im); filter(im); line(im,swr,s1);
+			l[0]=letters[l[0]]; l[1]=letters[l[1]]; l[2]=letters[l[2]]; l[3]=letters[l[3]]; l[4]=letters[l[4]];
+			close(f);
+			return 0;
+		}
+		else {
+			close(f);
+			return -EFAULT;
+		}
+	}
+	else
+		return -ENODEV;
 }
 
 #ifdef CAPTCHA
--- test.c
+++ test.c
@@ -1,5 +1,5 @@
 const int gifsize;
-void captcha(unsigned char im[70*200], unsigned char l[6]);
+int captcha(unsigned char im[70*200], unsigned char l[6]);
 void makegif(unsigned char im[70*200], unsigned char gif[gifsize]);
 
 int main() {
